4fef942d8d9cbd10da46e0a932c08b9b09a8101c
Akash Parkale <akashparkale1993@gmail.com>
2019-02-19 16:18:21 +0530

    Initial commit

---
+++ app/src/main/java/emaildemo/com/akash/emaildemo/config.java
@@ -0,0 +1,6 @@
+package emaildemo.com.akash.emaildemo;
+
+public class config {
+    public static final String EMAIL ="akashparkale1993@gmail.com";
+    public static final String PASSWORD ="Akash@1993";
+}
